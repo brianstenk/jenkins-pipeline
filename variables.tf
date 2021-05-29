de74af9f0fee695e3bd07ffced29b1bde9984099
jullio444 <jullio444@gmail.com>
2021-05-29 12:28:44 -0500

    changed key pair

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "instance_count" {
 
 variable "key_name" {
   description = "Private key name to use with instance"
-  default     = "terraform_kp"
+  default     = "amazon1"
 }
 
 variable "instance_type" {
